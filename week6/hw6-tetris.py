2c2deb2ddc48e40d643d02ecd559c2e911088620
Jigar Patel <jigarpatel95377@gmail.com>
2020-04-22 19:14:23 +0530

    Comlpeted step 6 changes, although as the game progress it becomes slow

--- week6/hw6-tetris.py
+++ week6/hw6-tetris.py
@@ -13,11 +13,13 @@ def fallingPieceIsLegal(app):
                     return False
     return True
 
+def removeFullRows():
+    pass
+
 def rotateFallingPiece(app):
     oldPieceDimensions = (len(app.fallingPiece), len(app.fallingPiece[0]))
     oldPiece = app.fallingPiece
-    oldRow = app.fallingPieceRow
-    oldCol = app.fallingPieceCol
+    oldRow, oldCol = app.fallingPieceRow, app.fallingPieceCol
     
     newRotatedPiece = []
     for i in range(len(app.fallingPiece[0])):
@@ -40,14 +42,20 @@ def rotateFallingPiece(app):
 def moveFallingPiece(app, drow, dcol):
     app.fallingPieceRow+=drow
     app.fallingPieceCol+=dcol
+    if not (fallingPieceIsLegal(app)):
+        app.fallingPieceRow-=drow
+        app.fallingPieceCol-=dcol
+        return False
+    return True
 
 def newFallingPiece(app):
     randomIndex = random.randint(0, len(app.tetrisPieces) - 1)
     app.fallingPiece = app.tetrisPieces[randomIndex]
     app.fallingPieceColor = app.tetrisPieceColors[randomIndex]
     app.fallingPieceRow = 0
-    app.numFallingPieceCols = len(app.fallingPiece[0])
-    app.fallingPieceCol = app.cols//2 - app.numFallingPieceCols//2
+    app.fallingPieceCol = app.cols//2 - len(app.fallingPiece[0])//2
+    if not (fallingPieceIsLegal(app)):
+        app.isGameOver = True
 
 def drawFallingPiece(canvas, app):
     for i in range(len(app.fallingPiece)):
@@ -69,63 +77,70 @@ def drawCell(app, canvas, row, col, color):
     y2 = app.margin + app.CellSize*row + app.CellSize
     canvas.create_rectangle(x1, y1, x2, y2,fill = color ,width=1)
 
-def redrawAll(canvas, app):
-    canvas.create_rectangle(0,0, app.width, app.height,fill = "orange" ,width=1)
-    drawBoard(canvas, app)
+def drawGameOver(canvas, app):
+    # Too lazy to not hardcord the values :P
+    canvas.create_rectangle(30,30,20+20*10,20+20*3,fill = "black" ,width=10)
+    canvas.create_text(125,50, font=("Purisa", 15),fill = "WHITE", text="GAME OVER!!", width=200)
 
+def redrawAll(canvas, app):
+    drawBoard(canvas, app)
     drawFallingPiece(canvas, app)
+    if app.isGameOver:
+        drawGameOver(canvas, app)
 
 def keyPressed(event, app):
-    if (event.keysym == "Up"):
-        rotateFallingPiece(app)
-        if not (fallingPieceIsLegal(app)):
+    if not app.isGameOver:
+        if (event.keysym == "Up"):
             rotateFallingPiece(app)
-    elif (event.keysym == "Down"):
-        moveFallingPiece(app,+1,0)
-        if not (fallingPieceIsLegal(app)):
-            moveFallingPiece(app,-1,0)
-    elif (event.keysym == "Left"):
-        moveFallingPiece(app,0,-1)
-        if not (fallingPieceIsLegal(app)):
-            moveFallingPiece(app,0,+1)
-    elif (event.keysym == "Right"):
-        moveFallingPiece(app,0,+1)
-        if not (fallingPieceIsLegal(app)):
+        elif (event.keysym == "Down"):
+            moveFallingPiece(app,+1,0)
+        elif (event.keysym == "Left"):
             moveFallingPiece(app,0,-1)
-    else:
-        newFallingPiece(app)
+        elif (event.keysym == "Right"):
+            moveFallingPiece(app,0,+1)
+        elif (event.keysym == "space"):
+            newFallingPiece(app)
+    
+    if (event.keysym == "r"):
+        appStarted(app)
 
-def runApp(width,height):
+def placeFallingPiece(app):
+    for i in range(len(app.fallingPiece)):
+        for j in range(len(app.fallingPiece[0])):
+            if(app.fallingPiece[i][j]):
+                app.board[i+app.fallingPieceRow][j+app.fallingPieceCol] = app.fallingPieceColor
+    newFallingPiece(app)
+
+def timerFired(app):
+    if not app.isGameOver:
+        if not (moveFallingPiece(app, +1, 0)):
+            placeFallingPiece(app)
+        
+
+def runApp(app,width,height):
     def redrawAllWrapper(canvas, app):
         redrawAll(canvas, app)
         canvas.update()
 
-    # def mousePressedWrapper(event, canvas, data):
-    #     mousePressed(event, data)
-    #     redrawAllWrapper(canvas, data)
+    def timerFiredWrapper(canvas, app):
+        timerFired(app)
+        redrawAllWrapper(canvas, app)
+        # pause, then call timerFired again
+        canvas.after(app.timerDelay, timerFiredWrapper, canvas, app)
 
     def keyPressedWrapper(event, canvas, app):
         keyPressed(event, app)
         redrawAllWrapper(canvas, app)
 
-    class Struct(object): pass
-    app = Struct()
-    appStarted(app)
-    app.height = height
-    app.width = width
     root = Tk()
     root.resizable(width=False, height=False) # prevents resizing window
     canvas = Canvas(root, width=width, height=height)
     canvas.configure(bd=0, highlightthickness=0)
+    canvas.create_rectangle(0,0, app.width, app.height,fill = "orange" ,width=1)
     canvas.pack()
-
-    # set up events
-    # root.bind("<Button-1>", lambda event:
-    #                         mousePressedWrapper(event, canvas, data))
     root.bind("<Key>", lambda event:
                             keyPressedWrapper(event, canvas, app))
-    redrawAllWrapper(canvas, app)
-    
+    timerFiredWrapper(canvas, app)
     root.mainloop()  # blocks until window is closed    
 
 def appStarted(app):
@@ -134,12 +149,6 @@ def appStarted(app):
     app.board = []
     for i in range(app.rows):
         app.board.append([app.emptyColor]*app.cols)
-
-    # pre-load a few cells with known colors for testing purposes
-    app.board[0][0] = "red" # top-left is red
-    app.board[0][app.cols-1] = "white" # top-right is white
-    app.board[app.rows-1][0] = "green" # bottom-left is green
-    app.board[app.rows-1][app.cols-1] = "gray" # bottom-right is gray       
     
     # Seven "standard" pieces (tetrominoes)
     iPiece = [
@@ -163,6 +172,7 @@ def appStarted(app):
         [  True,  True, False ],
         [ False,  True,  True ]]
 
+    app.isGameOver = False
     app.tetrisPieces = [ iPiece, jPiece, lPiece, oPiece, sPiece, tPiece, zPiece ]
     app.tetrisPieceColors = [ "red", "yellow", "magenta", "pink", "cyan", "green", "orange" ]
     # Define and draw the new falling piece
@@ -179,7 +189,13 @@ def playTetris():
     rows, cols, CellSize, margin = gameDimensions()
     width = cols*CellSize+2*margin
     height = rows*CellSize+2*margin
-    runApp(width=width, height=height)
+    class Struct(object): pass
+    app = Struct()
+    appStarted(app)
+    app.height = height
+    app.width = width
+    app.timerDelay = 1
+    runApp(app,width=width, height=height)
 
 def main():
     playTetris()
